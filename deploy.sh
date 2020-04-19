bd2bd1f2e88841a61f8dc1f39da869bbb1fd0f30
o621 <kato-seiya-rb@ynu.jp>
2020-04-19 16:18:10 +0900

    feat: 🎸 deploy.sh
    add fish and fisherman.

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 
 
 DOT_DIR="${HOME}/dotfiles"
-DOT_HTTP="https://github.com/kat06/dotfiles.git"
+DOT_HTTP="https://github.com/o621/dotfiles.git"
 
 
 if [ ! -d ${DOT_DIR} ] ; then
@@ -34,3 +34,25 @@ do
 done
 
 echo $(tput setaf 2)Deploy dotfiles complete!. ✔︎$(tput sgr0)
+
+echo "bat install..."
+cd ~
+curl -LJO https://github.com/sharkdp/bat/releases/download/v0.11.0/bat_0.11.0_amd64.deb
+sudo dpkg -i bat_0.11.0_amd64.deb
+
+echo $(tput setaf 2)bat install complete!. ✔︎$(tput sgr0)
+
+echo "exa install..."
+sudo apt install cargo
+cargo install exa
+echo $(tput setaf 2)exa install complete!. ✔︎$(tput sgr0)
+
+echo "fish-shell install..."
+sudo apt-add-repository ppa:fish-shell/release-3
+sudo apt-get update
+sudo apt-get install fish
+curl https://git.io/fisher --create-dirs -sLo ~/.config/fish/functions/fisher.fish
+fish
+fisher add oh-my-fish/theme-eden
+echo "exec fish" >> ~/.bash_profile
+echo $(tput setaf 2)fish install complete!. ✔︎$(tput sgr0)
